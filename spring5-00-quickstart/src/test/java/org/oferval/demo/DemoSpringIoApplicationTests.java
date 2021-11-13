86a32ed870136bdc0e2f0e94fd06b052757a83bc
Oscar Fernandez Vallejo <oscar.fernandez.vallejo.ext@ing.com>
2021-11-13 20:49:04 +0100

    Initial commit

---
+++ spring5-00-quickstart/src/test/java/org/oferval/demo/DemoSpringIoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oferval.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
