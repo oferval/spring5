c57c174d4707bc34aef10ee509142abdf02fe86c
Oscar Fernandez Vallejo <oscar.fernandez.vallejo.ext@ing.com>
2021-11-15 23:24:27 +0100

    spring.io RESTful web service

---
+++ spring5-00-quickstart/src/test/java/org/oferval/demo/controller/GreetingControllerTest.java
@@ -0,0 +1,31 @@
+package org.oferval.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GreetingControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+
+  @Test
+  void greeting() {
+
+    mockMvc.perform(get("/greeting"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect()
+  }
+
+}
